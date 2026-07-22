a527d0abb16b069d66e0b722399b0468a2b49fa6
LoggysGit <albertmetsler3008@gmail.com>
2026-07-22 16:55:03 +0500

    Dependency injection

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 """ Main app file """
 
-import queue
 import threading
+from queue import Queue
 
 from modules import lib, data_manager, engine, interface
 
-gui_command_buffer = queue.Queue()
-sys_command_buffer = queue.Queue()
-
-dm = data_manager.DataManager(lib.DB_PATH, gui_command_buffer)
-core = engine.Core(gui_command_buffer, dm)
-app = interface.App(gui_command_buffer, sys_command_buffer, dm)
-
-def system_thread():
+def system_thread(
+        sys_buffer: Queue,
+        gui_buffer: Queue,
+        data_manager_obj: data_manager.DataManager,
+        core_engine: engine.Core ):
     """ System thread """
+
     while True:
-        item = sys_command_buffer.get()
+        item = sys_buffer.get()
 
         try:
             command, payload = item
@@ -24,30 +22,30 @@ def system_thread():
                 # Analysis command #
                 case "RUN":
                     # Check DB relevance first
-                    dm.handle_disease_db_update()
+                    data_manager_obj.handle_disease_db_update()
 
                     lib.log("Analysing started...")
 
                     # Compare
                     data_file_path, gene_id = payload
-                    results = core.run_comparing(data_file_path, gene_id)
+                    results = core_engine.run_comparing(data_file_path, gene_id)
 
                     # Extract mutations
-                    full_mutations_data = core.find_mutations(results, gene_id)
+                    full_mutations_data = core_engine.find_mutations(results, gene_id)
                     full_mutations_data.sort(key=lambda r: r[0])
 
                     # Send results
                     for di in full_mutations_data:
-                        gui_command_buffer.put(("MUTATION", di))
+                        gui_buffer.put(("MUTATION", di))
 
                     # Mark for done
-                    gui_command_buffer.put(("DONE", None))
+                    gui_buffer.put(("DONE", None))
                     lib.log("Analysing has ended.")
 
                 # Export file command #
                 case "EXPORT":
                     mut_list, gene_ref, exp_dir = payload
-                    dm.save_mutations_to_vcf(exp_dir, mut_list, gene_ref)
+                    data_manager_obj.save_mutations_to_vcf(exp_dir, mut_list, gene_ref)
 
                 # Daemon close (break) command #
                 case "CLOSE":
@@ -57,17 +55,33 @@ def system_thread():
                 case _:
                     lib.dbg(f"Unknown command: {command}")
 
+        except ValueError:
+            lib.log(f"Wrong command structure (CMD, PAYLOAD): {item}")
+
         except Exception as e:
             lib.log(f"System thread error: {e}")
 
         finally:
-            sys_command_buffer.task_done()
+            sys_buffer.task_done()
 
 if __name__ == "__main__":
+    # App command queues
+    sys_command_buffer = Queue()
+    gui_command_buffer = Queue()
+
+    # App objects
+    dm = data_manager.DataManager(lib.DB_PATH, gui_command_buffer)
+    core = engine.Core(gui_command_buffer, dm)
+    app = interface.App(gui_command_buffer, sys_command_buffer, dm)
+
     # Start system daemon
-    sys_thread = threading.Thread(target=system_thread, daemon=True)
+    sys_thread = threading.Thread(
+        target=system_thread,
+        args=(sys_command_buffer, gui_command_buffer, dm, core),
+        daemon=True)
     sys_thread.start()
 
+    # Start app loop
     def on_closing():
         """ All-threads close function """
         lib.log("App closing...")
